03de9aac5c4157c55525f731d5b5d0a809c62794
Glen1986 <escobar.glen@gmail.com>
2022-06-06 11:11:55 -0300

    typescript-input-

--- index.ts
+++ index.ts
@@ -3,48 +3,13 @@ const suma = (a: number, b: number) => {
 }
 console.log(suma(1, 2))
 console.log('typescript')
-
-interface IAnimal {
-    nombre: string
-    tipo: 'terrestre' | 'marino'
-    rugir(volumen: number): void
-    domestico: boolean
-}
-
-interface IFelino extends IAnimal {
-    visionNocturna: boolean
-}
-interface ICanino extends IAnimal {
-    porte: 'pequeno' | 'medio' | 'grande'
-}
-
-const animal: IAnimal = {
-    nombre: 'elefante',
-    tipo: 'terrestre',
-    rugir: (volumen) => `${volumen}dB`,
-    domestico: true,
-}
-console.log(animal.rugir(8))
-
-const felino: IFelino = {
-    nombre: 'leon',
-    tipo: 'terrestre',
-    visionNocturna: true,
-    rugir: (volumen) => `${volumen}dB`,
-    domestico: true,
-}
-type IDomentico = ICanino | IFelino
-
-const myAnimal: IDomentico = {
-    domestico: true,
-    nombre: 'perro',
-    porte: 'medio',
-    tipo: 'terrestre',
-    visionNocturna: false,
-}
-
-console.log(animal)
-console.log(felino)
+//especificamos pata typescript que el elemento input
+//es un elemento HTML e incluye sus metodos html
+const input = document.getElementById('input') as HTMLInputElement
+input.addEventListener('input', (e) => {
+    const i = e.currentTarget as HTMLInputElement
+    console.log(i.value)
+})
 
 //interfaces son utilizadas para definir conratos de estructura de datos de clases
 //y los types para hacer mis de onterfaces en nuestro codigo
